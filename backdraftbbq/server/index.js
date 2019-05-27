5c573987f44583bb4ace0d355ca84afbba3f6758
Adam Martin <adam.madram@gmail.com>
2019-05-27 14:59:48 -0500

    Adding in basic auth

--- backdraftbbq/server/index.js
+++ backdraftbbq/server/index.js
@@ -1,5 +1,4 @@
 'use strict';
-"use strict";
 
 var fs = require("fs"),
     path = require("path"),
@@ -22,8 +21,36 @@ var serverPort = 8080;
 var spec = fs.readFileSync(path.join(__dirname, "api/swagger.yaml"), "utf8");
 var swaggerDoc = jsyaml.safeLoad(spec);
 
+var jwt = require('jsonwebtoken');
+var fs = require('fs');
+var cert_pub = fs.readFileSync(__dirname + '/config/backdraft-test.pem');
+function verifyToken(req, secDef, token, next) {
+    const bearerRegex = /^Bearer\s/;
+
+    if (token && bearerRegex.test(token)) {
+        var newToken = token.replace(bearerRegex, '');
+        jwt.verify(newToken, cert_pub,
+            {
+                algorithms: ["RS256", "HS256"],
+            },
+            (error, decoded) => {
+                if (error === null && decoded) {
+                    return next();
+                }
+                return next(req.res.sendStatus(403));
+            }
+        );
+    } else {
+        return next(req.res.sendStatus(403));
+    }
+}
+
 // swaggerRouter configuration
 var options = {
+    oasSecurity: true,
+    securityFile: {
+        Bearer: verifyToken
+    },
     controllers: path.join(__dirname, "./controllers"),
     useStubs: process.env.NODE_ENV === "development" // Conditionally turn on stubs (mock mode)
 };
--- backdraftbbq/src/App.js
+++ backdraftbbq/src/App.js
@@ -10,49 +10,57 @@ import AddImg from "./components/addimage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import GalleryApi from "./generated/src/api/GalleryApi";
+import Auth from './Auth.js';
+
+const auth = new Auth();
 const menuApi = new MenuItemApi();
 const galleryApi = new GalleryApi();
 
 class App extends Component {
-  state = { menuItems: [], gallery: [] };
-  componentDidMount() {
-    menuApi.getMenuItems((error, data, response) => {
-      console.log(data);
-      this.setState({ menuItems: data || [] });
-    });
-    galleryApi.getGalleryImages((error, data, response) => {
-      console.log(data);
-      this.setState({ gallery: data || [] });
-    });
-  }
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Switch>
-            <Route path="/admin/gallery/:id" component={AddImg} />
-            <Route path="/admin/gallery" component={AddImg} />
-            <Route path="/admin/menu/:id" component={AddMenuItem} />
-            <Route path="/admin/menu" component={AddMenuItem} />
-            <Route
-              path="/"
-              render={() => {
-                return (
-                  <React.Fragment>
-                    <Navbar />
-                    <About />
-                    <MenuList menuItems={this.state.menuItems} />
-                    <Gallery gallery={this.state.gallery} />
-                    <Contact />
-                  </React.Fragment>
-                );
-              }}
-            />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+    constructor(props) {
+        super(props);
+        const user = window.localStorage.getItem("user");
+        console.log(user);
+        this.state = { menuItems: [], gallery: [], user: user };
+    }
+    componentDidMount() {
+        menuApi.getMenuItems((error, data, response) => {
+            console.log(data);
+            this.setState({ menuItems: data || [] });
+        });
+        galleryApi.getGalleryImages((error, data, response) => {
+            console.log(data);
+            this.setState({ gallery: data || [] });
+        });
+    }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+					<Switch>
+						<Route path="/admin/gallery/:id" component={AddImg} />
+						<Route path="/admin/gallery" component={AddImg} />
+						<Route path="/admin/menu/:id" component={AddMenuItem} />
+						<Route path="/admin/menu" component={AddMenuItem} />
+						<Route
+						  path="/"
+						  render={() => {
+							return (
+							  <React.Fragment>
+								<Navbar />
+								<About />
+								<MenuList menuItems={this.state.menuItems} />
+								<Gallery gallery={this.state.gallery} />
+								<Contact />
+								<div onClick={() => auth.login()}>Login</div>
+							  </React.Fragment>
+							);
+						  }}
+						/>
+				  </Switch>
+				</div>
+			</BrowserRouter>
+        );
+    }
 }
-
 export default App;
