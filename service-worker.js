58885e371def02cc19952c2b41256a012e9e75aa
Trent <lowery.trent@gmail.com>
2020-07-18 09:27:51 -0700

    Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73d758420e40ed7599ed5b0da628fc80.js"
+  "/precache-manifest.a67bed2c9cc2385ecef6d55ae8c1e716.js"
 );
 
 workbox.clientsClaim();
